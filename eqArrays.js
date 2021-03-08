cefaecfa352d0a88b341175714019854c6e276e2
Ruowen Tang <ruowen.tang@gmail.com>
2021-03-08 13:53:07 -0500

    refactor eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  (actual === expected) ? console.log(`Assertion Passed: ${actual} === ${expected}`) : console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  
-};
-
 // first compare length of two arrays, return false if doesn't match
 // loop though array values and compare
 const eqArrays = function(arrayOne, arrayTwo) {
@@ -22,6 +16,4 @@ const eqArrays = function(arrayOne, arrayTwo) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 5, 3]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
