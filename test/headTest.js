ee36454a0510b97e7b5fe45969f9778674e61690
Ruowen Tang <ruowen.tang@gmail.com>
2021-03-08 14:46:05 -0500

    refactor headTest.js using mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,16 @@
-const assertEqual = require('../assertEqual');
+//const assertEqual = require('../assertEqual');
 const head = require('../head');
+const assert = require('chai').assert;
 
-assertEqual(head([1,2,4]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+});
+
